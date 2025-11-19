0efc9683624d1e60ea1e356dc4d980de93daafc4
Thore <thorekoopmans@gmail.com>
2025-11-19 20:44:18 +0100

    Added server to inspect page

--- src/main/java/org/carpenoctemcloud/controllers/FileInspectController.java
+++ src/main/java/org/carpenoctemcloud/controllers/FileInspectController.java
@@ -1,30 +1,14 @@
 package org.carpenoctemcloud.controllers;
 
-import org.carpenoctemcloud.redirect_files.RedirectFileCreator;
-import org.carpenoctemcloud.redirect_files.RedirectFileFactory;
-import org.carpenoctemcloud.redirect_files.RedirectFilePlatform;
 import org.carpenoctemcloud.remote_file.RemoteFile;
 import org.carpenoctemcloud.remote_file.RemoteFileService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Optional;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 /**
  * Collection of endpoints which handles sending a downloadable file to the client.
@@ -56,8 +40,10 @@ public class FileInspectController {
     @GetMapping({"/{id}", "/{id}/"})
     public String fileInspectPage(Model model, @PathVariable int id) {
         Optional<RemoteFile> file = fileService.getRemoteFileByID(id);
+        String server = file.get().downloadURL().replaceFirst("^file:/*", "").split("/")[0];
 
         model.addAttribute("resultFile", file.get());
+        model.addAttribute("serverURL", server);
 
         return "fileInspectPage";
     }
